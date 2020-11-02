bdd4bfc2401c9bebe1702ecfa0591b9db49fa6f3
Marcos8i22o <marcos.jinno@gmail.com>
2020-11-02 10:59:59 -0300

    agrega happy path

--- pruebas.js
+++ pruebas.js
@@ -1,5 +1,10 @@
 function probarValidarCantidadIntegrantes () {
     console.assert(
+        validarCantidadIntegrantes(2) === "",
+        "ValidarCantidadIntegrantes no validó un ingreso correcto"
+    );
+
+    console.assert(
         validarCantidadIntegrantes(0) === "Ingrese un número mayor a cero",
         "ValidarCantidadIntegrantes no validó que el campo sea mayor a cero",
     );   
